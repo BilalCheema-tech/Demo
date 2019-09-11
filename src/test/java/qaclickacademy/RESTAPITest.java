dd5d0d32c90e638c9ae80710167734f2a88f12a7
BilalCheema-tech <bilal.farooq.cheema@gmail.com>
2019-09-12 00:00:17 +1000

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	// trigger  testNg xml file from Maven
+	// how to execute all test cases from test folder mvn -test
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
